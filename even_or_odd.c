6bdfbd91a5d23407f80241ab83667d5ffda08c6f
randerson112358 <randerson112358@users.noreply.github.com>
2014-04-04 14:19:01 -0400

    Create even_or_odd.c

---
+++ even_or_odd.c
@@ -0,0 +1,19 @@
+/*
+  This program determines if a number (n) is even or odd
+  By: Rodney Anderson
+  4/4/2014
+*/
+
+# include<stdio.h>
+
+int main(void){
+	
+	int n = 16;
+	
+	if(n%2 == 0)
+	  printf("Even \n");
+	else
+	  printf("Odd \n");
+	  
+	system("pause");
+}
