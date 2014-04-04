9d5a59c62ad8414a8d5789da1bdf3fca0c400f0e
randerson112358 <randerson112358@users.noreply.github.com>
2014-04-04 14:07:23 -0400

    Create multiplication_table.c

---
+++ multiplication_table.c
@@ -0,0 +1,33 @@
+/*
+  This program generates a NxN multiplication  table
+  By: Rodney Anderson
+  4/4/2014
+*/
+
+# include<stdio.h>
+
+int main(void){
+	
+	int i , j;
+	int n = 16;
+	
+	//prints the columns
+	printf("%4c |", ' ');
+	for(i=0; i<=n; i++)
+	  printf("%4d", i);
+	  
+	  printf("\n");
+	
+	//Prints the Multiplication table
+	for(i=0; i<=n; i++)
+	  {
+	  	printf("%4d |", i); // prints the rows
+	  	for(j=0; j<=n; j++)
+	  	{
+	  		printf("%4d", i*j); //  %4d for aligning the text on screen
+	  	}
+	  	printf("\n");
+	  }
+	
+	system("pause");
+}
