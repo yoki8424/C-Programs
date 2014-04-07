77caec3de960e9e5e34d2bb258a05a31b01c24fc
randerson112358 <randerson112358@users.noreply.github.com>
2014-04-07 12:02:45 -0400

    Create factorial.c

---
+++ factorial.c
@@ -0,0 +1,46 @@
+/*
+
+ fact
+ This program outputs the factorial of a number e.g. 5! = 5 * 4 * 3 * 2 * 1 = 120.
+ In general n! = n * n-1 * n-2* ... * 3 * 2 * 1
+ By: Rodney Anderson
+ 
+ */
+
+# include<stdio.h>
+
+int fact_recursive(int n);// This is a recursive factorial function
+int fact_iterative(int n);// This is a iterative factorial function
+
+int main(void)
+{
+	int n;
+	
+	printf("Enter a number: ");
+	scanf("%d", &n);
+	
+	printf("%d! = %d\n", n, fact_iterative(n));
+	
+	system("pause");
+}
+
+fact_recursive(int n)
+{
+	//Base Case
+	if(n == 0)
+	return 1;
+	
+	return n *fact_recursive(n-1);
+}
+
+int fact_iterative(int n)
+{
+	int i;
+	int product = 1;
+	for(i = 1; i<= n; i++)
+	{
+		product = product * i;
+	}
+	
+	return product;
+}
