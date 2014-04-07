3a76cb23fad71874d5b9a891b1d4cb04b2682d5e
randerson112358 <randerson112358@users.noreply.github.com>
2014-04-07 14:25:39 -0400

    Create is_integer.c

---
+++ is_integer.c
@@ -0,0 +1,32 @@
+/*
+  This program determines if a number is an integer or not
+  By; Rodney Anderson
+  4/7/14
+  
+*/
+
+# include<stdio.h>
+
+int isInt(double n);
+
+int main(void)
+{
+	double n;
+	
+	printf("Input a number to determine if it is an integer or not: ");
+	scanf("%lf", &n);
+	
+	if(isInt(n) == 0)
+	 printf("%d is an integer\n", (int)n);
+	else
+	 printf("%lf is not an integer\n", n);
+	system("pause");
+}
+
+int isInt(double n)
+{
+	if((int) n - n == 0) // Then it's n is an integer
+	  return 0;
+	else // n is not an integer
+	  return 1;
+}
