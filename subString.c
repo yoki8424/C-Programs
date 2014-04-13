e1cee9bc1330b591cb7788fb664ad7ff2017f661
randerson112358 <randerson112358@users.noreply.github.com>
2014-04-13 17:57:42 -0400

    Create subString.c

---
+++ subString.c
@@ -0,0 +1,37 @@
+/*
+  This program prints all of the possible substrings of a word
+  and calculates the number of the substrings
+  By: Rodney Anderson
+  4/13/14
+*/
+
+# include<stdio.h>
+# include<string.h>
+
+int main(void)
+{
+    char* string = "ABCDEFGH" ;
+    int length = strlen(string);
+    int i,j,k;
+    
+    //Calculating the number of possible substrings
+    int numSubstring = ( length * (length +1) )/ 2;
+    
+    //Print the possible number of substrings
+    printf("The number of substrings in the word %s is %d\n\n", string,numSubstring);
+    
+    //Printing the string substrings
+    for(k=0; k<length; k++){
+     for(i=0; i<length; i++){
+       for(j=k; j<length-i; j++){
+               printf("%c",string[j]);
+               
+               if(j+1 >=  length-i)
+                 printf("\n");
+       }
+      }
+    printf("\n");
+    }
+    
+    system("pause");
+}
