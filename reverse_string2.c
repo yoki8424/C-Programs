99b2d62e2218ad6e6966dd93866607d0ceab5200
randerson112358 <randerson112358@users.noreply.github.com>
2016-07-25 15:22:56 -0400

    reverse_string2
    This program prints a string/word in reverse.

---
+++ reverse_string2.c
@@ -0,0 +1,24 @@
+/**
+ This program reverses the letters in a word
+*/
+# include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void printReverseStr(char* ch);
+
+int main(void){
+	char* word = "hello world";
+	printReverseStr(word); // This function reverse the string 
+
+}
+
+void printReverseStr(char* ch){
+    
+	int wordLength = strlen(ch);
+	
+	int i;
+	for(i=wordLength -1 ; i>= 0; i--){
+		printf("%c", ch[i]);
+	}
+}
