36e9bc06cbeb9c728f5cf3918a0c6ba56245e73e
randerson112358 <randerson112358@users.noreply.github.com>
2016-11-12 14:55:40 -0500

    Update containsStr.c

--- containsStr.c
+++ containsStr.c
@@ -1,44 +1,65 @@
 /*
   This program tells if a string is contained in another string.
-  Example: Does 'harbor' contain 'or' , since it is the code will print 'The word was contained in ....'
+  Example: Does 'harbor' contain 'or' , since it is the code will print 'Yes or is contained in harbor starting at position 4 of harbor', else it prints .. it is not contained in ..
+  By: randerson112358
 */
 
 # include <stdio.h>
 # include <string.h>
 # define LEN 31
 
+/*
+  This function returns the starting index (a value greater than or equal to 0) of the substring within the string, and returns -1 if the Str2 isn't contained in Str1
+*/
+int contains(char* str1, char* str2);
+
 int main(void)
-{//arr5 = (int**)malloc(5*sizeof(int));//allocating space for a 2d array
-    int i,j;
-    char *string1="abcdefg";//= (char*)malloc(5*sizeof(char));;
-    char *string2="a";//=(char*)malloc(5*sizeof(char));;
-    int strlen1=strlen(string1), strlen2=strlen(string2);   
-    int startingIndex;  
+{
     
-    /*printf("Insert string 1 to see if it contains string 2\n");
-    printf("String 1 = ");
-    scanf("%s", string1);
-    printf("String 2 = ");
-    scanf("%s", string2);*/
+    char *string1="harbor";
+    char *string2="ore";
+    int index = contains(string1, string2) ;
+    
+    // If index is greater than 0 then we know the starting index of the substring and that string2 is contained in string1
+    if( index >= 0)
+      printf("Yes %s is contained in %s starting at position %d of %s \n", string2, string1, index, string1 );
+    else
+      printf("%s is not contained in %s \n", string2,string1);
+    
+    
+    //Only use this if you are using a  Windows operating system, otherwise comment it out
+    system("pause");
+}
+
+
+/*
+  This function returns the starting index (a value greater than or equal to 0) of the substring within the string, and returns -1 if the Str2 isn't contained in Str1
+*/
+int contains(char* str1, char* str2){
+	
+	int i,j;
+	int strlen1=strlen(str1), strlen2=strlen(str2);   
+    int startingIndex = 0;  
     
     if(strlen1 < strlen2)
-    printf("%s is not contained in %s\n", string2,string1);
+       return -1;
     else
     {
         for(i=0; i<strlen1; i++)
          for(j=0; j< strlen2; j++)
          {
                   
-                  if(string1[i+j] == string2[j] )
+                  if(str1[i+j] == str2[j] )
                   {               startingIndex = i;  
                                 
                                  if(j== strlen2 -1)
-                                 printf("The word was contained in the first word at index %d\n", startingIndex);
+                                   return startingIndex;
                   }
                   else
                   break;
          }
-        
+        return -1;
     }
-    system("pause");
+    
+    
 }
