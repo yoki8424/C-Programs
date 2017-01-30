a2241ed24a7023a7d0ac7dd9edbc53a2ea586e59
randerson112358 <randerson112358@users.noreply.github.com>
2017-01-29 23:55:31 -0500

    Create if_else.c

---
+++ if_else.c
@@ -0,0 +1,19 @@
+# include <stdio.h>
+
+int main(){
+	
+	if(1 > 10){
+		printf("One is greater than 10 \n");
+	}
+	else if( 1 == 10){
+		printf("One is equal to 10 \n");
+	}
+	else if( 1 < 10){
+		printf("One is less than 10 \n");
+	}
+	else{
+		printf("One is NOT greater than 10 \n");
+	}
+	
+	return 0;
+}
