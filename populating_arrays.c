98fc98b058e03f4883d8ec3dda9b9d8872c6b4f2
randerson112358 <randerson112358@users.noreply.github.com>
2017-02-03 21:42:35 -0500

    Create populating_arrays.c

---
+++ populating_arrays.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+
+int main(){
+	
+	//Array defined
+	int numbers[10]= {9,20,30,40,50,60,70,80,90,100};
+	
+	//Populate the array
+	/*numbers[0] = 10;
+	numbers[1] = 20;
+	numbers[2] = 30;
+	numbers[3] = 40;
+	numbers[4] = 50;
+	numbers[5] = 60;
+	numbers[6] = 70;
+	numbers[7] = 80;
+	numbers[8] = 90;
+	numbers[9] = 100;*/
+	
+	//Print the 7th number in the array
+	printf("The 1st number in the array is %d", numbers[0]);
+	
+	return 0;
+}
