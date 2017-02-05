8150a4506717b98474afa465fdd16ca514e36ff0
randerson112358 <randerson112358@users.noreply.github.com>
2017-02-04 20:35:12 -0500

    Create forLoops.c

---
+++ forLoops.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+	
+	int i;
+	int array[10]={1,2,3,4,5,6,7,8,9,10};
+	int sum = 0;
+	
+	//Create our for loop
+	for(i=0; i < 10; i++){
+		
+		sum = sum + array[i];
+		
+		//If i reaches the last number before exiting the loop print '=' instead of '+'
+		if(i == 9){
+			printf("%d = ", array[i]);
+		}
+		else{
+			printf("%d + ", array[i]);
+		}
+	}
+	
+	//Print the sum of the array
+	printf("%d \n", sum);
+	
+	char *name = "Izzy";
+	int length = strlen(name);
+	
+	//Print every character in the array
+	for(i=0; i<length; i= i +1){
+		printf("%c \n", name[i]);
+	}
+	
+	//Infinite for loop
+	for( ; ; ){
+		//Code here
+		printf("hello \n");
+	}
+	
+	return 0;
+}
+
