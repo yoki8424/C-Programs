18d91f3926e28e2d91c6c27ab53095913ff76af9
randerson112358 <randerson112358@users.noreply.github.com>
2017-04-03 19:34:47 -0400

    Update isLeapYear.c

--- isLeapYear.c
+++ isLeapYear.c
@@ -1,50 +1,39 @@
+
+//Description: This program shows if a year is a leap year or not.
 /*
-  This program determines if a year is a leap year
+   How to know if a year is a Leap Year:
+yes|	Leap Years are any year that can be evenly divided by 4 (such as 2012, 2016, etc)
+no |	except if it can be evenly divided by 100, then it isn't (such as 2100, 2200, etc)
+yes|	except if it can be evenly divided by 400, then it is (such as 2000, 2400)
+
 */
 
 # include <stdio.h>
 
-// The start date of the first calendar
-# define  CALENDAR_START 1582
+int main(){
+	
+	int year;
 
+	printf("Please enter a year to check if it is a leap year\n");
+	scanf("%d", &year);
 
-int main(void)
-{
-  int year, month;
-  
-  // Get date information
-  printf("\nEnter year: ");
-  scanf("%d", &year);
- 
-  // Validate user input
-  if( (year < CALENDAR_START) )
-  {
-    printf("Bad Input: %d \n", year);
-    system("pause");
-    return; 
-  }
-  
-  
-  // Determine if the year is a leap year, 0 is false and 1 is true, default value is 0 for false
-  int isDivisibleBy4 = 0, isDivisibleBy100 = 0, isDivisibleBy400 = 0;
-  int isLeapYear = 0;
-  
-  if(year % 4 == 0)
-   isDivisibleBy4 = 1;
-  
-  if(year % 100 == 0)
-   isDivisibleBy100 = 1;
-  
-  if(year % 400 == 0)
-   isDivisibleBy400 = 1;
-   
-   if(isDivisibleBy4 == 1 || isDivisibleBy100 == 1 || isDivisibleBy400 == 1)
-     isLeapYear = 1;
-    
-   if(isLeapYear == 1) 
-   printf("%d is a leap year\n", year);
-   else
-    printf("%d is NOT a leap year\n", year);
-    
-     system("pause");
- }
+	if(year%400 == 0){
+		//Is leap year
+		printf("%d is a leap year \n", year);
+	}
+	else if(year % 100 == 0){
+		//is NOT leap year
+		printf("%d is NOT a leap year \n", year);
+	}
+	else if(year %4 == 0){
+		//is leap year
+		printf("%d is a leap year \n", year);
+	}
+	else
+	{
+		//is not leap year
+		printf("%d is NOT a leap year \n", year);
+	}
+
+	return 0;
+}
