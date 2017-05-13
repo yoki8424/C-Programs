fd454c24175a4c627d95937a1d64d83c1541cb6d
randerson112358 <randerson112358@users.noreply.github.com>
2017-05-13 12:07:23 -0400

    Update initials.c

--- initials.c
+++ initials.c
@@ -1,5 +1,9 @@
 /**
 * This program (initials.c), prints the initials of someones full name in all capitals
+* 
+*  USAGE: Please enter your name:
+          tommy pickles
+          Your initials are: TP
 */
 
 //Initializing the APIs/ Libraries
@@ -33,18 +37,20 @@ void getInitials(char* name){
      int i=0;
      
      // If the string length is greater than 0 then there is at least 1 character at index[0]
-     if(strlen(name) > 0)
+     // and if that character is alpha numeric it must be the first letter for the initials.
+     if(strlen(name) > 0 && isalpha(name[0]))
        printf("%c", toupper(name[0]));
      
      //While the current character in name does not equal the last character in name denoted by '\0' , 
      // go through each character of the string
+     //NOTE: Could've used i<=strlen(name) instead of name[i] != '\0' to accomplish the same task
      while(name[i] != '\0')
      {
                    //If the character is a space, then print the next character
-                   if(isspace(name[i]) && (i+1 < strlen(name)))
+                   if(isspace(name[i]) != 0)
                    {
                                        //Loops through all of the spaces for human input error
-                                       while(isspace(name[i])&& (i+1 < strlen(name)))
+                                       while(isspace(name[i])&& i <= strlen(name))
                                        {
                                              i++ ;             
                                        }
