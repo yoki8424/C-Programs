3b7ed4130f730668d175666a7dcc8f581dc25606
randerson112358 <randerson112358@users.noreply.github.com>
2017-05-20 12:56:00 -0400

    Update recursiveLog.c

--- recursiveLog.c
+++ recursiveLog.c
@@ -1,34 +1,36 @@
 /*
-   Description: This is a recursive logarithmic program
+   Description: This is a recusrsive logarithmic program
 */
 
-#include<stdio.h>
+# include <stdio.h>
 
-//Declare the recursive log function
-int log(int base, int val);
+//Declare the recurisve log function
+int logB(int base, int val);//log2(8) == 3 , 2 is our base, 8 is our val, 3 is the integer that we return
 
 int main(){
 	
+	int base = 2;
+	int value = 1024;
+	int answer = logB(base, value);
+	
+	printf("log base %d with value %d = %d \n", base, value, answer);
 	return 0;
 }
+
+
+//log2(1) = 0 ==> 2^0 = 1
 /*
-     log2(8) = 3 ==> 8/2 = 4
-                     4 /2 = 2
-                     2/2  = 1 is our base case
-                     
-     2^3 = 8 ==> 2 * 2 * 2 ==> 8
-     
-     
+   2^3 = 2 * 2 *2 = 8   <== 1 + log(8/2); ==> 1 + 2 = 3
+   log2(8) = 3, 8/2 = 4 <== 1 + log(4/2); ==> 1 + 1 = 2
+                4/2 = 2  <== 1 + log(2/2); == > 1 + 0= 1
+                2/2 = 1 <== our base case return 0
 */
-
-
-int log(int base, int val){
+int logB(int base, int val){
 	
 	//base case
 	if( val == 1)
 	  return 0;
 	  
-	//Recursive Case
-	return 1 + log(base, val/base);
-	
+	  //recursive case
+	  return 1 + logB(base, val/base);
 }
