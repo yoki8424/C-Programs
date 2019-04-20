8e04177b8c71f18eead0a91c9bd944e5267c6bf7
randerson112358 <randerson112358@users.noreply.github.com>
2019-04-20 00:06:24 -0400

    Update isPrime.c

--- isPrime.c
+++ isPrime.c
@@ -7,16 +7,15 @@ Note: Prime number (N) properties
    
 We only have to check up to sqrt(N) from 2, because if N is not prime it can be factored into two factors (a) and (b) such that N = a * b
 if a or b were greater than the square root of N then a*b would be greater than N. So the factors have to be less than or equal to the sqrt(N), and 2 is the next number after 1.
-
-By: Rodney Anderson
+By: Randerson112358
 4/7/2014
-
 */
 
 # include<stdio.h>
 # include<math.h> // has sqrt() function
 
-int isPrime(int N);// This returns a factor of N if it's prime and returns 0 other wise.
+// This returns a factor of N if it's composite (meaning a number greater than 1) or returns 1 if the number is prime.
+int isPrime(int N);
 
 int main(void)
 {
@@ -25,10 +24,12 @@ int main(void)
 	printf("Enter a number I will tell you if it is prime or composite: ");
 	scanf("%d", &N);
 	
-	if(isPrime)
-	  printf("Number %d is prime, one factor is %d\n", N, isPrime(N));
+
+	
+	if(isPrime(N) == 1)
+	  printf("Number %d is prime, the only factors are 1 and %d\n", N, N);
 	else
-	  printf("Number %d is composite, the only factors are 1 and %d\n", N, N);
+	  printf("Number %d is composite, one factor is %d\n", N, isPrime(N));
 	
 	system("pause");
 }
@@ -45,5 +46,5 @@ int isPrime(int N)
 	  
 	    
 	    
-	return 0;
+	return 1;
 }
